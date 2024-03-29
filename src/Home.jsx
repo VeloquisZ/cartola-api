8ddf60bc1b1c2b333f3c02ca7a1648587f92903d
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2024-03-29 19:46:17 -0300

    arrumando .map -

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,43 +1,47 @@
 import { Link } from "react-router-dom"
 import axios from "axios"
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
 import Jogadores from "./Jogadores.jsx"
+import './index.css';
 
-function Home (){
+function Home() {
 
-    const [times,setTimes] = useState(null)
+    const [times, setTimes] = useState([])
 
-    // useEffect( () =>{
-    //     buscarTimes()
-    // },[])
-    
-    
-    
+    useEffect( () =>{
+        buscarTimes()
+    },[])
+
+
+
     const buscarTimes = async () => {
-        const resultado  = await axios.get("https://api.cartola.globo.com/clubes")
-    setTimes(Object.values(resultado.data))
+        const resultado = await axios.get("https://api.cartola.globo.com/clubes")
+        let lista = Object.values(resultado.data)
+         console.log(lista)
+        setTimes(lista)
     }
-console.log(times)
 
-    return(
-        
+
+    return (
+
         <div>
             <img src="https://logodownload.org/wp-content/uploads/2017/05/cartola-fc-logo-5.png"
                 className='cartola'></img>
             <br></br>
+            {times.length == 0 ? <h1>Carregando</h1> : <></>}
             {
-                listarTimes.length > 0 && (
+                times.length >= 1 && (
                     <ul>
                         {times.map((item, index) =>
                             <li key={index}>
-                                <div>   
-                                    <Link to={`/Jogadores/${item.id}/${item.nome}`}>
-                                        <img src={item?.escudos['60x60']}></img>
+                                <div>
+                                    <Link to={`/Jogadores`}>
+                                        <img src={item.escudos['30x30']}></img>
                                     </Link>
                                 </div>
-                                <div className='nomeTime'>
-                                    <p> <strong>{item?.nome} </strong></p>
-                                    <p> {item?.apelido}  </p>
+                                <div>
+                                    <p> <strong>{item.nome} </strong></p>
+                                    <p> {item.apelido}  </p>
                                 </div>
 
                             </li>
@@ -48,12 +52,12 @@ console.log(times)
                 )
             }
         </div>
-        
-        
-        
-    
+
+
+
+
     )
-    
+
 
 }
 
